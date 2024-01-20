0ce21e5524378ef5759d6d666f27f4bc584deff1
Mithun s <chakravarthimithun62@gmail.com>
2024-01-21 02:58:37 +0530

    'read-write-files'

---
+++ index.js
@@ -0,0 +1,28 @@
+const fs = require('fs')
+
+fs.readFile('./files/starter.txt',(err,data) => {
+    if(err) throw err;
+    console.log(data)     //the data is given out as a buffer data(u cant read and understand it as it is not encoded)
+    console.log(data.toString())
+})
+//another way to read the file is 
+fs.readFile('./files/starter.txt','utf8',(err,data) => {     //an extra parameter UTF-8 is used  -->'utf8' is an encoding option. 
+    if(err) throw err;          //When reading or writing files, it's essential to specify the character encoding to interpret the binary data correctly.
+    console.log(data)           //'utf8' is one of the most common character encodings, representing Unicode Transformation Format 8-bit.
+})
+//best way to read the files is :-
+const path = require('path')
+fs.readFile(path.join(__dirname,'files','starter.txt'),"utf8",(err,data) => {
+    if (err) throw err;
+    console.log(data)
+})
+
+
+//exit on uncaught errors        
+process.on('uncaughtException', err => {   //The 'on' method is used to register event listeners for specific events in Node.js. In this case, it's used to listen for the 'uncaughtException' event.
+    console.log(`there was an uncaught error: ${err}`)     //process is a global object in Node.js that provides information about, and control over, the current Node.js process. It allows you to interact with various aspects of the runtime environment
+    process.exit(1)  //The callback function provided to process.on takes an err parameter, which represents the uncaught exception that occurred.
+})                  //The 'uncaughtException' event is emitted when an unhandled exception occurs in the Node.js process.
+
+console.log('hello....')   //u can see that hello is console.loged first before the readfiles operation defining its asyncronous(non-blocking -> not waiting for any opertaion to completely finish but then execute next operations and come back their)
+
---
+++ index2.js
@@ -0,0 +1,79 @@
+const fs = require('fs')
+const path = require('path')
+
+fs.readFile(path.join(__dirname,'files','starter.txt'),"utf8",(err,data) => {
+    if (err) throw err;
+    console.log('data is read ; \n ',  data)
+})
+
+console.log('helllo...')
+
+fs.writeFile(path.join(__dirname,'files','reply.txt'),"nice to meet u Mithun",(err) => {
+    if (err) {
+        console.log(err)
+        return
+    }
+    console.log('write is complete')
+})
+
+fs.appendFile(path.join(__dirname,'files','text.txt')," \n testing text",(err) => {
+    if (err) throw err;          //NOTE:- while using append file if the given file does'nt exist then it will create a new file of the given name and add the data to that file.
+    console.log('append to text file is complete')     //note the sequence of operation(read, write,append) performed in the terminal.
+})  
+
+// fs.rename(path.join(__dirname,'files','reply.txt'),path.join(__dirname,'files','reply_renamed.txt'),(err) => {
+//     if (err) throw err;          
+//     console.log('rename complete')    //renaming a file
+// }) 
+
+
+fs.writeFile(path.join(__dirname,'files','reply.txt'),"nice to meet u Mithun",(err) => {
+    if (err) throw err;
+    console.log('write is complete2')
+    fs.appendFile(path.join(__dirname,'files','reply.txt'),"\n yes it is",(err) => {
+        if (err) throw err;          
+        console.log('append complete2') 
+        fs.rename(path.join(__dirname,'files','reply.txt'),path.join(__dirname,'files','reply_renamed.txt'),(err) => {
+            if (err) throw err;          
+            console.log('rename complete2')   
+        }) 
+    })  //now this is looking like a callback hell
+})
+
+//checking if a file exists
+const fileExists = fs.existsSync('example.txt');
+console.log('File exists : ', fileExists)
+
+//working with directory
+fs.readdir(path.join(__dirname), (err, files) => {  
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log('Files in the directory:', files);
+  });
+//making/creating a directory
+fs.mkdir('newDirectory', (err) => {  //(go to files and check their will be a new diectory created in the folder)
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log('Directory created.');
+    // Removing a directory
+    fs.rmdir('newDirectory', (err) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        console.log('Directory removed.');
+      });
+  });
+
+
+
+
+//exit on uncaught errors        
+process.on('uncaughtException', err => {   
+    console.log(`there was an uncaught error: ${err}`)     
+    process.exit(1)  
+})
